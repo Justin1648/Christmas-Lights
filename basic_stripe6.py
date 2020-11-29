5bcacd91e08f6ec7be2939b416b708d4e6193dc9
Justin1648 <35910949+Justin1648@users.noreply.github.com>
2020-11-28 22:18:57 -0700

    Add files via upload

---
+++ basic_stripe6.py
@@ -0,0 +1,74 @@
+# Two color stripe moves from lower to upper number pixels
+
+import time
+import board
+import neopixel
+
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+
+pixel_pin = board.D18
+
+# The number of NeoPixels
+
+num_pixels = 300
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+
+ORDER = neopixel.RGB
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, 
+    auto_write=False, pixel_order=ORDER)
+
+# Preset color codes
+RED = (0, 255, 0)
+ORANGE = (79, 251, 20)
+YELLOW = (255, 255, 0)
+GREEN = (255, 0, 0)
+BLUE = (0, 0, 255)
+PURPLE = (0, 128, 128)
+WHITE = (255, 255, 255)
+OFF = (0, 0, 0)
+
+def stripe6(color1, color2, color3, color4, color5, color6, window, iterations):
+    for start in range(iterations):
+        for window_start in range(0, num_pixels, window):
+            for i in range(window):
+                if (window * (1/6)) >= i:
+                    color = color1
+                elif (window * (2/6)) >= i > (window * (1 / 6)):
+                    color = color6
+                elif (window * (3/6)) >= i > (window * (2/6)):
+                    color = color5
+                elif (window * (4/6)) >= i > (window * (3/6)):
+                    color = color4
+                elif (window * (5/6)) >= i > (window * (4/6)):
+                    color = color3
+                else:
+                    color = color2
+                pixels[(start + window_start + i) % num_pixels] = color
+            pixels.show()
+            time.sleep(.001)
+
+#Run Program
+
+if __name__ == '__main__':
+
+    try:
+
+        while True:
+
+        #stripeChase Red/White (Color1, Color2, length of stripe, iterations)
+            print ('Red/White stripe')
+            stripe6(RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, 100, 100000)
+
+        #stripeChase Red/Green
+#            print ('Red/Green stripe')
+#            stripe3(RED, GREEN, WHITE, 30, 100)
+
+
+    except KeyboardInterrupt:
+        pixels.fill(OFF)
+        pixels.show()
+
+
